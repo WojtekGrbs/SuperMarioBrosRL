2c0949b26d0fc3831dd18a19450dea6d41b9e0a3
Wojtasauce <51636941+WojtekGrbs@users.noreply.github.com>
2024-05-27 22:54:21 +0200

    Nagroda za ukończenie poziomu

--- rewards.py
+++ rewards.py
@@ -4,9 +4,9 @@ def calculate_reward_waste_of_time(info, previous_info):
 	# 1. Nagroda za przemieszczanie się w prawo
 	#if 2*(info['x_pos'] - previous_info['x_pos'])>-100:
 	#	reward += 2*(info['x_pos'] - previous_info['x_pos'])
-	# 2. Nagroda za ukończenie poziomu
-	if info['flag_get']:
-		reward += 500
+	
+	# 2. Nagroda za ukończenie poziomu 
+	# Uwzgledniona zostala w preparations.py, domyslnie zawsze jest wlaczona w postaci reward += 500
 
 	# 3. Nagroda za unikanie przeszkód (przeskoczenie rury)
 	#if info['status'] == 'tall' and previous_info['status'] == 'small':
